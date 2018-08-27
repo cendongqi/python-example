41ab6745b3627b13e027a73307261db90da35afa
tanzhe <tanzhe@phone580.com>
2018-08-27 20:13:30 +0800

    初始化项目

---
+++ example/data_type.py
@@ -0,0 +1,47 @@
+print(123)
+
+print(1.2)
+
+print(1.2e9)
+
+print("I\'m OK")
+print("I'm OK")
+
+print('\\\n\\')
+
+print(r'\\\n\\')
+
+# print('''123
+# 123
+# 123
+# 123
+# 123''')
+
+print('''
+123
+123
+123
+1231
+2312
+31
+231
+3
+123
+1
+3
+12''')
+
+print(r'''
+123
+1231\\\\\n\n\n\n\n\n
+23123
+1231
+231
+23
+123
+1''')
+
+
+print(True and False)
+print(True or False)
+print(not True)
---
+++ example/input_output.py
@@ -0,0 +1,19 @@
+# output
+print('hello', 'python')
+print('hello' + ' python')
+
+# input
+# name = input()
+# print(name)
+
+# demo1
+# name = input()
+# print('hello', name)
+
+# demo2
+age = input("请输入你的年龄:")
+print('你的年龄是:', age)
+
+# exercise
+# 请利用print()输出1024 * 768 = xxx：
+print('2014 * 768 =', 1024 * 768)
